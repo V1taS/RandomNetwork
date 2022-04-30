9ff9a756d8885c2cc6ccf870764b7153135c3645
Sosin Vitalii <375693@mail.ru>
2022-04-30 13:46:33 +0300

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "RandomNetwork",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "RandomNetwork",
+            targets: ["RandomNetwork"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -12,7 +20,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "RandomNetwork",
             dependencies: []),
         .testTarget(
---
+++ Sources/RandomNetwork/TestViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TestViewController.swift
+//  RandomNetwork
+//
+//  Created by Vitalii Sosin on 30.04.2022.
+//
+
+import Foundation
+
+class TestViewController {
+    
+}
